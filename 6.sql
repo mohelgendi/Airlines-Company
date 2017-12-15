6b409a7659852b34096973b2e830dc882fbb0dff
ChrisOscar <winchesterchriss22@gmail.com>
2017-12-15 18:08:34 +0300

    Add files via upload

--- 6.sql
+++ 6.sql
@@ -1,30 +1,4 @@
-	/*10 SELECT STATEMENTS*/
-/*    5b i.  USING 2 TABLES************************************/
-/*selecting all the aiports an airplane can lnad on*/
-SELECT airplane.Airplane_Id, can_land.Airport_Code 
-FROM can_land,airplane
-WHERE airplane.Airplane_Type = can_land.Airplane_Type_Name
-
-/*selecting the airport name and city a plane is departing from*/
-SELECT airport.Name, airport.City
-FROM airport,flight_leg
-WHERE flight_leg.Departure_Airport_Code = airport.Airport_Code
-AND flight_leg.Leg_Number = 2
-AND flight_leg.Flight_Number = "VL455"
-
-/*selecting all the that depart from LAX Airport*/
-SELECT flight.Flight_Number 
-from flight,flight_leg
-where flight.Flight_Number = flight_leg.Flight_Number
-AND flight_leg.Departure_Airport_Code = "LAX"
-
-
-/*END OF USING 2 TABLES************************************/
-
-/* 5b ii. USING 3 TABLES************************************/
-
-
-						/*VIEWS*/
+/*VIEWS*/
 /*GETS THE NAMES AND DESTINATION OF THE CUSTOMER*/
 create view Customer_Departure_Arrival as
 select seat_reservation.Customer_Name,D.City AS DEPARTURE_FROM,A.City AS ARRIVE_AT
@@ -43,7 +17,7 @@ order by seat_reservation.Customer_Name;
 
 /*GETS CUSTOMERS FLIGHT, DEPARTURE TIME AND ARRIVAL AIRPORT TO REMINDER LATE CUSTOMRESOF THEIR FLIGHTS*/
 create view customer_departure_reminder as
-SELECT seat_reservation.Customer_Name,seat_reservation.Flight_Number,leg_instance.Departure_Time,airport.Name
+SELECT seat_reservation.Customer_Name,seat_reservation.Flight_Number,leg_instance.Departure_Time,airport.Name as GOING_TO
 FROM seat_reservation,leg_instance,airport
 WHERE (seat_reservation.Flight_Number,seat_reservation.Leg_Number,seat_reservation.Date) =
 (leg_instance.Flight_Number,leg_instance.Leg_Number,leg_instance.Date)
